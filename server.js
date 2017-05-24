87f41bb1633946991bfbb7b819ff5231fe12bb77
tungduong <vn.duongthanhtung@gmail.com>
2017-05-24 23:07:55 +0700

    ff

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ http.createServer(function (req, res) {
 	res.setHeader('Access-Control-Request-Method', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
 	res.setHeader('Access-Control-Allow-Headers', '*');
-	
+	console.log('request');
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     req.on('data', function (chunk) {
         console.log('GOT DATA! : ');
